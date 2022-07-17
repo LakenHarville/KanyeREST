c1b1e36cd15bf6999c9b93197460d1bf40c45da0
Laken Harville <rlakenharville@gmail.com>
2022-07-17 00:45:41 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeREST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var quote = new QuoteGenerator(client);
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye: {quote.Kanye()}");
+                Console.WriteLine("-----");
+                Console.WriteLine($"Ron: {quote.Ron()}");
+                Console.WriteLine("-----");
+            }
+        }
+    }
+}
---
+++ QuoteGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace KanyeREST
+{
+    public class QuoteGenerator
+    {
+
+        private HttpClient _client;
+
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+        public string Kanye()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string Ron()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse);
+
+            return ronQuote[0].ToString();
+        }
+    }
+}
